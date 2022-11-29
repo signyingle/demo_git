0a2970ba1562f84b6136b63ee796f554dd9e61d6
SIE_吴俊乐 <wujunle@chinasie.com>
2022-11-29 19:49:10 +0800

    tijiao 2

--- src/main/java/com/git/text/GitText.java
+++ src/main/java/com/git/text/GitText.java
@@ -2,6 +2,7 @@ package com.git.text;
 
 public class GitText {
     public static void main(String[] args) {
-        System.out.println("hahahhaha");
+        System.out.println("text git 1");
+        System.out.println("text git 2");
     }
 }
